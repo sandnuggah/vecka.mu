f0a6e676473586bf1e6a4ab92f7b64c576808381
Adam Agnaou <adam@fapfap.se>
2012-08-28 15:03:31 +0200

    added xml to api formats

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+require 'builder'
 
 get '/' do
 	haml :vecka
@@ -7,6 +8,12 @@ end
 
 get '/api/v1/week.json' do
 	content_type :json
-	
 	{:week => Time.new.strftime('%W')}.to_json
+end
+
+get '/api/v1/week.xml' do
+	builder do |xml|
+		xml.instruct!
+		xml.week Time.new.strftime('%W') 
+	end
 end
