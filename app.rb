e9af08d62662e982f45fe714f78a7b528af74ca3
Adam Agnaou <adam@fapfap.se>
2012-08-30 02:29:28 +0200

    converted tabs to spaces

--- app.rb
+++ app.rb
@@ -3,17 +3,17 @@ require 'json'
 require 'builder'
 
 get '/' do
-	haml :vecka
+  haml :vecka
 end
 
 get '/api/v1/week.json' do
-	content_type :json
-	{:week => Time.new.strftime('%W')}.to_json
+  content_type :json
+  {:week => Time.new.strftime('%W')}.to_json
 end
 
 get '/api/v1/week.xml' do
-	builder do |xml|
-		xml.instruct!
-		xml.week Time.new.strftime('%W') 
-	end
+  builder do |xml|
+    xml.instruct!
+    xml.week Time.new.strftime('%W') 
+  end
 end
