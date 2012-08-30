94ea268be95ff1ef2cbb012333b723d7b5303334
Adam Agnaou <adam@fapfap.se>
2012-08-30 13:48:09 +0200

    use one instance var instead of calling Time.new each time. Added a content type for the xml respons.

--- app.rb
+++ app.rb
@@ -3,17 +3,19 @@ require 'json'
 require 'builder'
 
 get '/' do
+  @week = Time.now.strftime('%W')
   haml :vecka
 end
 
 get '/api/v1/week.json' do
   content_type :json
-  {:week => Time.new.strftime('%W')}.to_json
+  {:week => Time.now.strftime('%W')}.to_json
 end
 
 get '/api/v1/week.xml' do
+  content_type :xml
   builder do |xml|
     xml.instruct!
-    xml.week Time.new.strftime('%W') 
+    xml.week Time.now.strftime('%W') 
   end
 end
