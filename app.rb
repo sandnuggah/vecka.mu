9a4092d0abe4e5273d657b81c570e9a51fa3dfa4
Adam Agnaou <adam@fapfap.se>
2013-01-07 14:23:01 +0100

    no more zero padding in front of single number weeks (1-9)

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@ require 'json'
 require 'builder'
 
 get '/' do
-  @week = Time.now.strftime('%W')
+  @week = Time.now.strftime('%W').to_i
   haml :vecka
 end
 
 get '/api/v1/week.json' do
   content_type :json
-  {:week => Time.now.strftime('%W')}.to_json
+  {:week => Time.now.strftime('%W').to_i}.to_json
 end
 
 get '/api/v1/week.xml' do
   content_type :xml
   builder do |xml|
     xml.instruct!
-    xml.week Time.now.strftime('%W') 
+    xml.week Time.now.strftime('%W').to_i
   end
 end
