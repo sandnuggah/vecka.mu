d239a05a29d640aa1952824e1e1cce288c4a6a04
Adam Agnaou <adam@fapfap.se>
2013-01-14 11:29:27 +0100

    solid week number implementation

--- app.rb
+++ app.rb
@@ -1,21 +1,23 @@
 require 'sinatra'
 require 'json'
 require 'builder'
+require 'date'
 
 get '/' do
-  @week = Time.now.strftime('%W').to_i
+  #@week = Time.now.strftime('%W').to_i
+  @week = Date.today.cweek.to_i
   haml :vecka
 end
 
 get '/api/v1/week.json' do
   content_type :json
-  {:week => Time.now.strftime('%W').to_i}.to_json
+  {:week => Date.today.cweek.to_i}.to_json
 end
 
 get '/api/v1/week.xml' do
   content_type :xml
   builder do |xml|
     xml.instruct!
-    xml.week Time.now.strftime('%W').to_i
+    xml.week Date.today.cweek.to_i
   end
 end
